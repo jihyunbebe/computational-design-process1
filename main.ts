cacfbbd202863e7a7ba92c2b5f2b028063d66ff0
jihyunbebe <117896478+jihyunbebe@users.noreply.github.com>
2022-11-10 17:19:31 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,32 +1,30 @@
-input.onButtonPressed(Button.A, function () {
-    basic.showLeds(`
-        . # . # .
-        # # # # #
-        # # # # #
-        . # # # .
-        . . # . .
-        `)
-    music.playTone(262, music.beat(BeatFraction.Quarter))
-    music.playTone(330, music.beat(BeatFraction.Quarter))
-    music.playTone(392, music.beat(BeatFraction.Quarter))
-})
-input.onGesture(Gesture.Shake, function () {
-    for (let index = 0; index < 3; index++) {
-        basic.showIcon(IconNames.SmallDiamond)
-        basic.showIcon(IconNames.Target)
-        basic.showIcon(IconNames.Diamond)
+let lightson = false
+input.onSound(DetectedSound.Loud, function () {
+    lightson = !(lightson)
+    if (lightson) {
+        basic.showIcon(IconNames.EigthNote)
+        music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
+        basic.showString("" + (lightson))
+    } else {
+        basic.clearScreen()
     }
-    music.playSoundEffect(music.builtinSoundEffect(soundExpression.twinkle), SoundExpressionPlayMode.UntilDone)
 })
-input.onLogoEvent(TouchButtonEvent.Released, function () {
-    music.stopAllSounds()
-})
-input.onButtonPressed(Button.AB, function () {
-    basic.clearScreen()
-})
-input.onButtonPressed(Button.B, function () {
-    basic.showString("love you")
-})
-input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
-    music.ringTone(262)
+basic.forever(function () {
+    if (input.lightLevel() > 100) {
+        basic.showLeds(`
+            # . # . #
+            . # # # .
+            # # # # #
+            . # # # .
+            # . # . #
+            `)
+    } else {
+        basic.showLeds(`
+            . # # # .
+            # # . . #
+            # . . . .
+            # # . . #
+            . # # # .
+            `)
+    }
 })
